7b085e49d8c72917c2ae15982379fb4e7bf96582
Vasi Logeswaran <Vasi.Logeswaran@gmail.com>
2016-08-01 16:55:10 +0100

    Changed Config to Singleton

--- config/Config.php
+++ config/Config.php
@@ -7,15 +7,27 @@ class Config
 {
     protected $fileSystem;
 
-    public function __construct(Filesystem $fileSystem)
+    private static $instance;
+    private static $repository;
+
+    public static function getInstance()
+    {
+        if (null === static::$instance) {
+            static::$instance = new Config(new \Illuminate\Filesystem\Filesystem);
+        }
+
+        return static::$repository;
+    }
+
+    protected function __construct(Filesystem $fileSystem)
     {
         $this->fileSystem = $fileSystem;
+        static::$repository = $this->make();
     }
 
     public function make()
     {
         $configFileArrays = $this->getConfigFileArrays();
-
         return new Repository($configFileArrays);
     }
 
@@ -23,17 +35,18 @@ class Config
     {
         $files = $this->fileSystem->files(__DIR__);
 
-        return $this->getArraysFromFiles($files); 
+        return count($files) > 0 ? $this->getArraysFromFiles($files) : [] ;
     }
 
     private function getArraysFromFiles(array $files)
     {
+        $result = [];
         foreach ($files as $file){
             if (!strpos($file, 'Config.php')){
-	        $result[$this->fileSystem->name($file)] = require_once $file;
+	             $result[$this->fileSystem->name($file)] = require_once $file;
             }
         }
-        
+
         return $result;
     }
 }
--- public/wp-content/mu-plugins/init.php
+++ public/wp-content/mu-plugins/init.php
@@ -1,9 +1,9 @@
 <?php
 
-$config = new Config\Config(new Illuminate\Filesystem\Filesystem);
+$respository = Config\Config::getInstance();
 
-$data = $config->make();
+error_log( print_r($respository->get('base.basic.password'), 1));
 
-foreach ($data->get('mu-plugins') as $plugin){
+foreach ($respository->get('mu-plugins') as $plugin){
     require_once __DIR__ . '/' . $plugin;
 }
